359ca48a5aaa7ea62bb049e722ca61e1481c76b0
zeropanic <zeropanic@myself.com>
2013-07-16 22:42:24 +0200

    Added documentation.

--- Traits/ArrayAccess.php
+++ Traits/ArrayAccess.php
@@ -1,25 +1,53 @@
 <?php
 
+/**
+ * This trait allows you to easily implement the ArrayAccess class of the SPL.
+ * @package  Traits
+ * @author  zeropanic <zeropanic@myself.com>
+ * @version  1.0
+ * @namespace Oz\Traits
+ */
+
 namespace Oz\Traits;
 
 trait ArrayAccess
 {
-    
+    /**
+     * offsetGet is a getter for offsets.
+     * @param  [string] $offset the offset you want to get
+     * @return [mixed]          the content of the offset
+     */
     public function offsetGet($offset)
     {
         return $this->{$offset};
     }
 
+    /**
+     * offsetSet is a setter for offsets.
+     * @param  string $offset      the offset you want to set
+     * @param  mixed  $offsetValue the value of the offset
+     * @return void
+     */
     public function offsetSet($offset, $offsetValue)
     {
         $this->{$offset} = $offsetValue;
     }
 
+    /**
+     * offsetExists check whether and offset exists or not.
+     * @param  string $offset the offset you want to check
+     * @return boolean        true if the offset exists, false if not
+     */
     public function offsetExists($offset)
     {
         return isset($this->{$offset});
     }
     
+    /**
+     * offsetUnset allows you to unset an offset.
+     * @param  string $offset the offset you want to unset
+     * @return void
+     */
     public function offsetUnset ($offset)
     {
         unset($this->{$offset});
